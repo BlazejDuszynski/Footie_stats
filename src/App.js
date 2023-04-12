85e714e272e695fa3a09cb9e7d0a1f5223eba5ba
Blazej Duszynski <bduszynski92@gmail.com>
2023-04-12 19:33:17 +0200

    installed react router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,19 @@
 import "./App.css";
+import Bundesliga from "./Components/Pages/Bundesliga";
+import Home from "./Components/Pages/Home";
+import PremierLeague from "./Components/Pages/PremierLeague";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    part: "/",
+    children: [
+      { path: "/", element: <Home /> },
+      { path: "/PremierLeague", element: <PremierLeague /> },
+      { path: "/Bundesliga", element: <Bundesliga /> },
+    ],
+  },
+]);
 
 function App() {
   return <div className="App">aaa</div>;
---
+++ src/Components/Pages/Bundesliga.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Bundesliga = () => {
+  return <div></div>;
+};
+
+export default Bundesliga;

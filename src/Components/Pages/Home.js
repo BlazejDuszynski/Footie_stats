04b83a52182948382d6f41b62fbf67fe75a81868
Blazej Duszynski <bduszynski92@gmail.com>
2023-04-17 22:22:45 +0200

    added welcome paragraph to Home.js

--- src/Components/Pages/Home.js
+++ src/Components/Pages/Home.js
@@ -2,18 +2,27 @@ import React, { Fragment } from "react";
 import classes from "./Home.module.css";
 
 const Home = () => {
-  const style = {
+  const h1style = {
     fontStyle: "italic",
     color: "var(--orangeFont)",
   };
 
+  const paragraphStyle = {
+    color: "var(--blueFont)",
+  };
+
   return (
     <Fragment>
-      <section className={classes.homeSection}>
+      <section className={classes.welcomeSection}>
         <h1 className={classes.welcome}>
-          <span style={style}>Footie Stats</span> - all TOP 5 european leagues
+          <span style={h1style}>Footie Stats</span> - all TOP 5 european leagues
           in one place!
         </h1>
+        <p className={classes.welcomeParagraph}>
+          <span style={paragraphStyle}>Footie Stats </span> - web app where
+          football fan can check match results, schedule and statistics of his
+          favourite team or player of the best european leagues.
+        </p>
       </section>
       <section></section>
     </Fragment>

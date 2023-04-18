9096921b8ed0b57f0577b15ff028777ccaee7df0
Blazej Duszynski <bduszynski92@gmail.com>
2023-04-18 21:32:31 +0200

    added links to leagues grid items

--- src/Components/Pages/Home.js
+++ src/Components/Pages/Home.js
@@ -30,35 +30,35 @@ const Home = () => {
           CHOOSE A LEAGUE
         </header>
         <div className={classes.leaguesGridContainer}>
-          <Link className={classes.gridItem}>
+          <Link to="premierleague/matches" className={classes.gridItem}>
             <img
               className={classes.leagueImage}
               src="https://e0.365dm.com/18/12/2048x1152/skysports-graphic-salah-sterling_4529251.jpg"
             ></img>
             <p className={classes.leagueName}>Premier League</p>
           </Link>
-          <Link className={classes.gridItem}>
+          <Link to="laliga/matches" className={classes.gridItem}>
             <img
               className={classes.leagueImage}
               src="https://pbs.twimg.com/media/FfMG2GIXgAIRsIB.jpg:large"
             ></img>
             <p className={classes.leagueName}>La Liga</p>
           </Link>
-          <Link className={classes.gridItem}>
+          <Link to="bundesliga/matches" className={classes.gridItem}>
             <img
               className={classes.leagueImage}
               src="https://fantomsportsindustries.com/wp-content/uploads/2023/02/skysports-bundesliga-bayern_6067185.jpg"
             ></img>
             <p className={classes.leagueName}>Bundesliga</p>
           </Link>
-          <Link className={classes.gridItem}>
+          <Link to="seriea/matches" className={classes.gridItem}>
             <img
               className={classes.leagueImage}
-              src="https://theanalyst.com/wp-content/uploads/2023/02/rafael-leao-stats-milan-1024x715.jpg"
+              src="https://assets.goal.com/v3/assets/bltcc7a7ffd2fbf71f5/bltef5ed04b33c80b7b/62f3dc7b00626810a9be53d6/cf5305df-3783-4d04-82c4-e3fd66b69155.jpg"
             ></img>
             <p className={classes.leagueName}>Serie A</p>
           </Link>
-          <Link className={classes.gridItem}>
+          <Link to="ligue1/matches" className={classes.gridItem}>
             <img
               className={classes.leagueImage}
               src="https://e0.pxfuel.com/wallpapers/478/35/desktop-wallpaper-psg-match-schedule-when-is-lionel-messi-playing-for-paris-saint-germain-sporting-news-messi-neymar-mbappe-psg.jpg"

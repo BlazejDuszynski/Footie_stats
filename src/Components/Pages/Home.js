daef5d9805d34cd4643fcb6f829229c4469d1731
Blazej Duszynski <bduszynski92@gmail.com>
2023-04-18 19:44:03 +0200

    added league photos and styled

--- src/Components/Pages/Home.js
+++ src/Components/Pages/Home.js
@@ -21,7 +21,7 @@ const Home = () => {
         </h1>
         <p className={classes.welcomeParagraph}>
           <span style={paragraphStyle}>Footie Stats </span> - web app where
-          football fan can check match results, schedule and statistics of his
+          football fan can check match results, schedules and statistics of his
           favourite team or player of the best european leagues.
         </p>
       </section>
@@ -31,19 +31,39 @@ const Home = () => {
         </header>
         <div className={classes.leaguesGridContainer}>
           <Link className={classes.gridItem}>
-            <p>Premier League</p>
+            <img
+              className={classes.leagueImage}
+              src="https://e0.365dm.com/18/12/2048x1152/skysports-graphic-salah-sterling_4529251.jpg"
+            ></img>
+            <p className={classes.leagueName}>Premier League</p>
           </Link>
           <Link className={classes.gridItem}>
-            <p>Premier League</p>
+            <img
+              className={classes.leagueImage}
+              src="https://pbs.twimg.com/media/FfMG2GIXgAIRsIB.jpg:large"
+            ></img>
+            <p className={classes.leagueName}>La Liga</p>
           </Link>
           <Link className={classes.gridItem}>
-            <p>Premier League</p>
+            <img
+              className={classes.leagueImage}
+              src="https://fantomsportsindustries.com/wp-content/uploads/2023/02/skysports-bundesliga-bayern_6067185.jpg"
+            ></img>
+            <p className={classes.leagueName}>Bundesliga</p>
           </Link>
           <Link className={classes.gridItem}>
-            <p>Premier League</p>
+            <img
+              className={classes.leagueImage}
+              src="https://theanalyst.com/wp-content/uploads/2023/02/rafael-leao-stats-milan-1024x715.jpg"
+            ></img>
+            <p className={classes.leagueName}>Serie A</p>
           </Link>
           <Link className={classes.gridItem}>
-            <p>Premier League</p>
+            <img
+              className={classes.leagueImage}
+              src="https://i.pinimg.com/736x/f8/22/7c/f8227c9c6000f3f53af29b07acde9c98.jpg"
+            ></img>
+            <p className={classes.leagueName}>Ligue 1</p>
           </Link>
         </div>
       </section>

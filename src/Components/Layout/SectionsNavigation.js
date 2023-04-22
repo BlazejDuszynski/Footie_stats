d74a255ff1ea1d957bc056d4235c90c000c9075e
Blazej Duszynski <bduszynski92@gmail.com>
2023-04-22 12:37:36 +0200

    installed react icons

--- src/Components/Layout/SectionsNavigation.js
+++ src/Components/Layout/SectionsNavigation.js
@@ -1,16 +1,21 @@
 import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import LeagueContext from "../Store/league-context";
+import classes from "./SectionsNavigation.module.css";
+
 
 const SectionsNavigation = () => {
   const { league } = useContext(LeagueContext);
 
   return (
-    <div>
-      <Link to={`/${league}/matches`}>Matches</Link>
+    <aside className={classes.asideNavigation}>
+      <Link to={`/${league}/matches`}>
+        <p>TbSoccerField</p>
+        <p>Matches</p>
+      </Link>
       <Link to={`/${league}/table`}>Table</Link>
       <Link to={`/${league}/statistics`}>Statistics</Link>
-    </div>
+    </aside>
   );
 };
 

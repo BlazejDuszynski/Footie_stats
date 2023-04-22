684a0721d331404c9de423fd6591858f0bde0225
Blazej Duszynski <bduszynski92@gmail.com>
2023-04-22 12:58:39 +0200

    styled subpage navbar

--- src/Components/Layout/SectionsNavigation.js
+++ src/Components/Layout/SectionsNavigation.js
@@ -2,19 +2,39 @@ import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import LeagueContext from "../Store/league-context";
 import classes from "./SectionsNavigation.module.css";
-
+import { TbSoccerField } from "react-icons/tb";
+import { IoStatsChart } from "react-icons/io5";
+import { RiListOrdered } from "react-icons/ri";
 
 const SectionsNavigation = () => {
   const { league } = useContext(LeagueContext);
 
   return (
     <aside className={classes.asideNavigation}>
-      <Link to={`/${league}/matches`}>
-        <p>TbSoccerField</p>
+      <Link
+        className={classes.asideNavigationItemLink}
+        to={`/${league}/matches`}
+      >
+        <p>
+          <TbSoccerField />
+        </p>
         <p>Matches</p>
       </Link>
-      <Link to={`/${league}/table`}>Table</Link>
-      <Link to={`/${league}/statistics`}>Statistics</Link>
+      <Link className={classes.asideNavigationItemLink} to={`/${league}/table`}>
+        <p>
+          <RiListOrdered />
+        </p>
+        <p>Table</p>
+      </Link>
+      <Link
+        className={classes.asideNavigationItemLink}
+        to={`/${league}/statistics`}
+      >
+        <p>
+          <IoStatsChart />
+        </p>
+        <p>Statistics</p>
+      </Link>
     </aside>
   );
 };

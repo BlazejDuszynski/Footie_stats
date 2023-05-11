b6e7b075fba1e5c6e0a4eef22028865593128a4e
Blazej Duszynski <bduszynski92@gmail.com>
2023-05-11 16:17:24 +0200

    created fetch function to get teams short names

--- src/Components/Pages/SubNavigationItems/Matches/MatchesItem.js
+++ src/Components/Pages/SubNavigationItems/Matches/MatchesItem.js
@@ -1,7 +1,43 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import classes from "./MatchesItem.module.css";
 
 const MatchesItem = (props) => {
+  const [loading, setLoading] = useState(false);
+  const [homeTeamShortName, setHomeTeamShortName] = useState();
+  const [awayTeamShortName, setAwayTeamShortName] = useState();
+
+  const options = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "72cff716cdmshc41548afe41ba07p18c95cjsn9521d9d88440",
+      "X-RapidAPI-Host": "api-football-v1.p.rapidapi.com",
+    },
+  };
+
+  console.log(props.homeTeamId);
+
+  const getTeamsShortNames = async () => {
+    setLoading(true);
+    const homeTeamResponse = await fetch(
+      "https://v3.football.api-sports.io/teams?id=" + props.homeTeamId,
+      options
+    );
+    const awayTeamResponse = await fetch(
+      "https://v3.football.api-sports.io/teams?id=" + props.awayTeamId,
+      options
+    );
+    const homeTeamData = await homeTeamResponse.json();
+    const awayTeamData = await awayTeamResponse.json();
+    console.log(homeTeamData, awayTeamData);
+    setHomeTeamShortName(homeTeamData.response.team.code);
+    setAwayTeamShortName(awayTeamData.response.team.code);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    getTeamsShortNames();
+  }, [props]);
+
   return (
     <div className={classes.matchesItem}>
       <div title={props.homeTeamName} className={classes.teamInfo}>

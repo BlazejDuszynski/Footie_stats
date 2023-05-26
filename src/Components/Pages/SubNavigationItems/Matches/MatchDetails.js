be17dbecb499df96fb876112a91bbc6c445b6154
Blazej Duszynski <bduszynski92@gmail.com>
2023-05-26 22:06:06 +0200

    applied function which toggles active class of tab names

--- src/Components/Pages/SubNavigationItems/Matches/MatchDetails.js
+++ src/Components/Pages/SubNavigationItems/Matches/MatchDetails.js
@@ -1,21 +1,34 @@
 import React from "react";
-import TabContext from '@mui/lab/TabContext';
+import classes from "./MatchDetails.module.css";
+import { useState } from "react";
 
 const MatchDetails = () => {
+  const [activeTab, setActiveTab] = useState(1);
+  const toggleActiveTab = (id) => {
+    setActiveTab();
+  };
   return (
     <div>
-      <TabContext value={value}>
-        <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
-          <TabList onChange={handleChange} aria-label="lab API tabs example">
-            <Tab label="Item One" value="1" />
-            <Tab label="Item Two" value="2" />
-            <Tab label="Item Three" value="3" />
-          </TabList>
-        </Box>
-        <TabPanel value="1">Item One</TabPanel>
-        <TabPanel value="2">Item Two</TabPanel>
-        <TabPanel value="3">Item Three</TabPanel>
-      </TabContext>
+      <header className={classes.matchDetailsHeader}>
+        <ul>
+          <li
+            className={activeTab === 1 ? classes.active : null}
+            onClick={() => {
+              setActiveTab(1);
+            }}
+          >
+            Lineups
+          </li>
+          <li
+            className={activeTab === 2 ? classes.active : null}
+            onClick={() => {
+              setActiveTab(2);
+            }}
+          >
+            Statistics
+          </li>
+        </ul>
+      </header>
     </div>
   );
 };
